78ce77d9eef8717689ad9d6c4e2a92d4bcfdae62
xxbabykinz67xx <francessandridge@protonmail.com>
2026-07-23 18:23:46 -0500

    added nerd fonts

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,7 @@ xdg.portal = {
 	wget
 	git
 	gh
+    nerd-fonts.jetbrains-mono
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 # Hyprland ecosystem
