6b8c15b4496c2b6acd7025189beb13cde122ecbb
xxbabykinz67xx <francessandridge@protonmail.com>
2026-07-23 22:47:31 -0500

    adding mpv and vlc

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,8 @@ xdg.portal = {
     fastfetch
     pavucontrol
     brave
+    mpv
+    vlc
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 # Hyprland ecosystem
